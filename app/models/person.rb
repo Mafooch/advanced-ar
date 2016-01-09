e280faf509af7c0f6f5418c90ff7aba554d61dc9
Mafooch <joshuamaffuccio@gmail.com>
2016-01-09 13:31:28 -0600

    add relation and seeds

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,6 @@
 class Person < ActiveRecord::Base
-  has_one :personal_info
+  has_one :personal_info, dependent: :destroy
   has_many :jobs
+  has_many :my_jobs, class_name: "Job"
+  # basically just creating a alias
 end
