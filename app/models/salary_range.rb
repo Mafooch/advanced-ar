b0da0ccf7db9a69fa19e8b04b62a462f56cea73e
Mafooch <joshuamaffuccio@gmail.com>
2016-01-10 20:31:35 -0600

    add validations to SalaryRange

--- app/models/salary_range.rb
+++ app/models/salary_range.rb
@@ -1,3 +1,13 @@
 class SalaryRange < ActiveRecord::Base
   belongs_to :job
+
+  validate :min_is_less_than_max
+  # we can actually do this usually numericality
+
+  def min_is_less_than_max
+    if min_salary > max_salary
+      errors.add(:min_salary, "cannot be greater than maximum salary!")
+      # adding an error by specifying error column and error message
+    end
+  end
 end
